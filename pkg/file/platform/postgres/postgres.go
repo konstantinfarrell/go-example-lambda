8b78068ea37063f05e2fd0741d33918cb1d3719e
Konstantin Farrell <konstantinfarell@gmail.com>
2021-05-18 18:56:31 -0700

    Fix postgres call invokation

--- pkg/file/platform/postgres/postgres.go
+++ pkg/file/platform/postgres/postgres.go
@@ -8,8 +8,8 @@ import (
 
 func CreateFile(d *postgres.Database, f *golx.File) (*golx.File, error) {
 	spname := "create_file"
-
-	_, err := d.Call(spname, f.Filename, f.Path, f.Permissions, f.Created, f.Modified, f.FileId, f.Data, f.Received)
+	var files []golx.File
+	_, err := d.Call(false, files, spname, f.Filename, f.Path, f.Permissions, f.Created, f.Modified, f.FileId, f.Data, f.Received)
 	if err != nil {
 		log.Printf("Error while calling create_file: %s", err)
 		return nil, err
@@ -19,8 +19,8 @@ func CreateFile(d *postgres.Database, f *golx.File) (*golx.File, error) {
 
 func DeleteFile(d *postgres.Database, f *golx.File) (error) {
 	spname := "delete_file"
-
-	_, err := d.Call(spname, f.FileId)
+	var files []golx.File
+	_, err := d.Call(false, files, spname, f.FileId)
 	if err != nil {
 		log.Printf("Error while calling delete_file: %s", err)
 		return err
